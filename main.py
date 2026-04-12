debd66c51a2ad3c45a52f77b47e7de0225c00c2b
ITUSaccident21 <24120472@student.hcmus.edu.vn>
2026-04-12 07:39:55 +0700

    Hoàn thành

--- main.py
+++ main.py
@@ -6,19 +6,19 @@ app = FastAPI()
 
 
 detector = pipeline("object-detection", model="yainage90/fashion-object-detection")
-# 1. Endpoint giới thiệu
+
 @app.get("/")
 def root():
-    return {"message": "API Nhận diện thời trang của Trí"}
+    return {"message": "API Nhận diện trang phục của Trí"}
 
 @app.post("/predict")
 async def predict(file: UploadFile = File(...)):
-    # 1. Đọc file ảnh từ người dùng gửi lên
+   
     image_data = await file.read()
     image = Image.open(io.BytesIO(image_data))
     
-    # 2. Đưa ảnh vào AI để nhận diện
+
     results = detector(image)
     
-    # 3. Trả kết quả về dạng JSON
+  
     return {"objects_detected": results}
--- test_api.py
+++ test_api.py
@@ -1,26 +1,25 @@
 import requests
 
-# 1. Cấu hình các thông số
+
 URL = "http://127.0.0.1:8000/predict"
-IMAGE_PATH = "Test.jpeg"  # Đảm bảo file này nằm cùng thư mục với code
+IMAGE_PATH = "Test2.jpeg"  
 
 def test_fashion_api():
     print(f"--- Đang gửi ảnh '{IMAGE_PATH}' tới API ---")
     
     try:
-        # 2. Mở file ảnh ở chế độ binary (rb)
+       
         with open(IMAGE_PATH, "rb") as f:
-            # Gửi request POST kèm file
-            # 'file' là tên tham số mà bạn đặt trong main.py (UploadFile)
+         
             files = {"file": (IMAGE_PATH, f, "image/jpeg")}
             response = requests.post(URL, files=files)
 
-        # 3. Kiểm tra mã trạng thái (Status Code)
+   
         if response.status_code == 200:
             print("✅ Kết nối thành công!")
             data = response.json()
             
-            # 4. Hiển thị kết quả đẹp mắt
+       
             print("\n[KẾT QUẢ NHẬN DIỆN]")
             objects = data.get("objects_detected", [])
             
